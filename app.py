47ab85374d30a3c1985f3eb4be70b1a3aaf94651
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2023-06-16 09:37:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,13 @@ import xgmodel
 from xgmodel import calculate_xG
 from xgmodel import xgfix
 
+with st.expander("Baca ini dulu."):
+    st.write(\"\"\"
+        Aplikasinya kelihatan error karena kedua file yang diperlukan belum diupload, upload dulu.
+        Untuk file timeline, pastikan tambahkan kolom X, Y, dan GW dulu. Format excelnya gak usah ada
+        yang diganti, ya.
+    \"\"\")
+
 col1, col2 = st.columns(2)
 
 with col1:
