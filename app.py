138adeeede987fc1f69ceab56ef5416ade8002bb
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2023-07-22 14:44:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -226,7 +226,7 @@ st.download_button(label='Download Data Excel+xG!',
                    file_name='Player+xG_'+team1+'vs'+team2+'.csv',
                    mime='text/csv')
 
-data.loc[28, 'Event'] = 'Shot Off'
+data.loc[29, 'Event'] = 'Shot Off'
 
 #Attempts Map
 fig, ax = plt.subplots(figsize=(20, 20), dpi=500)
