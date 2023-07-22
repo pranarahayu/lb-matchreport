6cd414f5cdf4862dde38c388b145a8c856d01872
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2023-07-22 14:29:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -226,7 +226,7 @@ st.download_button(label='Download Data Excel+xG!',
                    file_name='Player+xG_'+team1+'vs'+team2+'.csv',
                    mime='text/csv')
 
-fixdata.loc[(fixdata['X'].str.contains('90') & fixdata['Y'].str.contains('50')), 'Action'] = 'Shot Off'
+fixdata.loc[0, 'Action'] = 'Shot Off'
 
 #Attempts Map
 fig, ax = plt.subplots(figsize=(20, 20), dpi=500)
