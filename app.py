964b9bf33f978f9c9ffb7e470b1a9c025fcb8864
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2023-07-22 14:33:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -226,8 +226,6 @@ st.download_button(label='Download Data Excel+xG!',
                    file_name='Player+xG_'+team1+'vs'+team2+'.csv',
                    mime='text/csv')
 
-fixdata.loc[1, 'Action'] = 'Shot Off'
-
 #Attempts Map
 fig, ax = plt.subplots(figsize=(20, 20), dpi=500)
 pitch = VerticalPitch(half=True, pitch_type='wyscout', corner_arcs=True,
